665bca518738a6f2e009d90e39c0a1790637f57e
bthuerman <bthuerman@gmail.com>
2025-06-01 16:24:13 -0500

    Switching site to Eleventy version

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Copy images, CSS, JS, admin, and static folders directly to the output
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/script.js");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/media");
+
+  return {
+    dir: {
+      input: "src",     // Source files here
+      output: "_site"   // Built site output here
+    }
+  };
+};
